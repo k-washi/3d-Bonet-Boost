aab0a5726a7e2379efdce8c28aa8c9e32ba08a6f
k-washi <kai.washizaki@gmail.com>
2020-03-11 11:36:24 +0900

    Add access colab shell

---
+++ access_colab.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in `seq 0 12`
+do
+  echo "[$i]" ` date '+%y/%m/%d %H:%M:%S'` "connected."
+  open "https://colab.research.google.com/drive/1m0cyB0_JpfLUr4955llrboSNWaCyouGX?hl=ja#scrollTo=Y3zQ_I5Qg5tB"
+  sleep 3600
+done
